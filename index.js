5b42c3cdf18e0c351e0f487becc41931a08f666d
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2025-06-23 14:36:50 -0400

    Fixed JS to work on any project.html page properly.

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 console.log('Hello, World!');
 
+const url = window.location.href;
+
 const leftArrow = document.getElementById('project1-left')
 const rightArrow = document.getElementById('project1-right');
 const screenshot = document.getElementById('project-screenshot')
 
-const pics = [
-  './screenshots/Project1/all.png',
-  './screenshots/Project1/Gaming.png',
-  './screenshots/Project1/Music.png'
-];
+const pics = {
+  project1: [
+    './screenshots/Project1/all.png',
+    './screenshots/Project1/Gaming.png',
+    './screenshots/Project1/Music.png' 
+  ],
+  project2: [
+    './screenshots/Project2/gpt.png',
+    './screenshots/Project2/mdn.png',
+    './screenshots/Project2/vscode.png' 
+  ],
+  
+}
 
 let index = 0;
+let projectId = '';
+
+if ( url.includes('project1') ) {
+  projectId = 'project1';
+
+} else if ( url.includes('project2') ) {
+  projectId = 'project2';
+}
+
+screenshot.src = pics[projectId][index]
 
 function picsIndex () {
   switch ( index ) {
@@ -23,16 +43,17 @@ function picsIndex () {
   }
 }
 
+
 leftArrow.addEventListener( 'click', () => {
   index--;
   picsIndex()
-  screenshot.src = pics[index];
+  screenshot.src = pics[projectId][index];
   console.log(`you clicked left, Index is: ${index}`);
 })
 
 rightArrow.addEventListener('click', () => {
   index++
   picsIndex();
-  screenshot.src = pics[index]
+  screenshot.src = pics[projectId][index];
   console.log(`you clicked right, Index is: ${index}`);
 });
